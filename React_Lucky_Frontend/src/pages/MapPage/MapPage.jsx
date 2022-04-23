368cebfb1fcf176948c444b5ce8423203d289b97
Luis Martínez <wifly.sk8@gmail.com>
2022-04-23 17:42:04 +0200

    add map page

--- React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
+++ React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Nav from "../../shared/components/Nav/Nav";
+import Map from "../../assets/Estados/map.png";
+import SearchLogo from "../../assets/Buscador/lupa.png";
+import Filter from "../../assets/Buscador/filtro.png";
+
+import "./MapPage.scss";
 
 const MapPage = () => {
   return (
-    <div>
-      MapPage
-      <Nav />
-    </div>
+    <section className="c-map">
+      <div className="c-map__div">
+        <input type="text" className="c-map__input" placeholder="¿Qué estás buscando?" />
+        <img className="c-map__logo" src={SearchLogo} alt="lupa" />
+        <img className="c-map__logo c-map__logo--filter" src={Filter} alt="lupa" />
+        <img className="c-map__img" src={Map} />
+        <Nav />
+      </div>
+    </section>
   );
 };
 
