406560070eeefc9ca03dc6669a9f7ee598aa74e6
Luis Martínez <wifly.sk8@gmail.com>
2022-04-24 23:37:55 +0200

    add google maps api

--- React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
+++ React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
@@ -1,10 +1,18 @@
-import React from "react";
 import Nav from "../../shared/components/Nav/Nav";
-import Map from "../../assets/Estados/map.png";
+import "./MapPage.scss";
 import SearchLogo from "../../assets/Buscador/lupa.png";
 import Filter from "../../assets/Buscador/filtro.png";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
-import "./MapPage.scss";
+const containerStyle = {
+  width: "415px",
+  height: "700px",
+};
+
+const center = {
+  lat: 43.36,
+  lng: -8.4,
+};
 
 const MapPage = () => {
   return (
@@ -13,7 +21,12 @@ const MapPage = () => {
         <input type="text" className="c-map__input" placeholder="¿Qué estás buscando?" />
         <img className="c-map__logo" src={SearchLogo} alt="lupa" />
         <img className="c-map__logo c-map__logo--filter" src={Filter} alt="lupa" />
-        <img className="c-map__img" src={Map} alt="logo" />
+        <LoadScript googleMapsApiKey="AIzaSyDv2W4zkf-OMEH7-arAoe55APu-U0JX-ZU">
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
         <Nav />
       </div>
     </section>
