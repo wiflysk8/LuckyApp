9b99b9f5ecf4c176bae7b7f66d9da128136cbe23
Luis Martínez <wifly.sk8@gmail.com>
2022-04-24 02:28:33 +0200

    add filter image to pet site, add favicon and change app title

--- React_Lucky_Frontend/src/shared/components/Card/Card.jsx
+++ React_Lucky_Frontend/src/shared/components/Card/Card.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Card.scss";
+import Fav from "../../../assets/PerfilAnimales/favoritos.png";
 
 const Card = ({ mascota }) => {
   return (
-    <div className="c-card">
-      <img className="c-card__img" src={mascota.imagen} alt={mascota.nombre} />
-      <div className="c-card__footer">
-        <h3 className="c-card__name">{mascota.nombre}</h3>
-        <div className="c-card__footer-right">
-          <p className="c-card__footer-right__p">{mascota.ciudad}</p>
-          <p className="c-card__footer-right__p c-card__footer-right__p--right">{mascota.peso}Km</p>
+    <>
+      {" "}
+      <img className="c-card__fav" src={Fav} alt="hearth" />
+      <div className="c-card">
+        <img className="c-card__img" src={mascota.imagen} alt={mascota.nombre} />
+        <div className="c-card__footer">
+          <h3 className="c-card__name">{mascota.nombre}</h3>
+          <div className="c-card__footer-right">
+            <p className="c-card__footer-right__p">{mascota.ciudad}</p>
+            <p className="c-card__footer-right__p c-card__footer-right__p--right">{mascota.peso}Km</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
