0f8093124e2cb8e3e19826f662853fcb100dd5cc
Luis Martínez <wifly.sk8@gmail.com>
2022-04-24 02:57:00 +0200

    add image to user model

--- React-Lucky_Backend/app/models/user.js
+++ React-Lucky_Backend/app/models/user.js
@@ -9,6 +9,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true
     },
+    image: {
+      type: String,
+      required: false
+    },
     email: {
       type: String,
       validate: {
