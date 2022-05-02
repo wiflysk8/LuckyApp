f295645616a843d64d8258191889b176805c954c
Claarainiesta7 <iniesta.llop@bootcamp-upgrade.com>
2022-05-02 13:06:02 +0200

    popmaps

--- React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
+++ React_Lucky_Frontend/src/pages/MapPage/MapPage.jsx
@@ -4,6 +4,9 @@ import SearchLogo from "../../assets/Buscador/lupa.png";
 import Filter from "../../assets/Buscador/filtro.png";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import React from "react";
+import CustomPopup from "../../shared/components/CustomPopup/CustomPopup";
+import { useState } from "react";
+import PopMaps from "../../shared/components/PopMaps/PopMaps";
 
 const containerStyle = {
   width: "100vw",
@@ -16,12 +19,25 @@ const center = {
 };
 
 const MapPage = () => {
+
+  const [visibility, setVisibility] = useState(false);
+
+  const popupCloseHandler = (e) => {
+    setVisibility(e);
+  };
+
+
   return (
     <section className="c-map">
       <div className="c-map__div">
         <input type="text" className="c-map__input" placeholder="¿Qué estás buscando?" />
         <img className="c-map__logo" src={SearchLogo} alt="lupa" />
-        <img className="c-map__logo c-map__logo--filter" src={Filter} alt="lupa" />
+        <img className="c-map__logo c-map__logo--filter" src={Filter} alt="lupa" onClick={(e) => {
+              setVisibility(!visibility);
+              e.preventDefault();
+              //navigate("/estado");
+              //window.location.reload();
+            }} />
         <div className="c-map__googleMaps">
           <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
             <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
@@ -31,6 +47,9 @@ const MapPage = () => {
           </LoadScript>
         </div>
         <Nav />
+        <CustomPopup onClose={popupCloseHandler} show={visibility} title="">
+            <PopMaps />
+        </CustomPopup>
       </div>
     </section>
   );
