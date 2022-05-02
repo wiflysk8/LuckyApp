472597622784d80b23edd9410c55e9bced34539f
Luis Martínez <wifly.sk8@gmail.com>
2022-05-02 20:32:58 +0200

    del clogs

--- React_Lucky_Frontend/src/shared/components/AdopcionDetail/AdopcionDetail.jsx
+++ React_Lucky_Frontend/src/shared/components/AdopcionDetail/AdopcionDetail.jsx
@@ -21,7 +21,6 @@ const AdopcionDetail = () => {
     const getMascota = async () => {
       const res = await axios.get(`http://localhost:5000/mascotas/${nombre}`);
       setMascotaDetail(res.data);
-      console.log(res.data);
     };
 
     getMascota();
