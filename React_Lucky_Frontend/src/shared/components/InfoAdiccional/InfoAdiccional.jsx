85b93f99e59d3d3606d77c74a088cac3fe851fd7
Luis Martínez <wifly.sk8@gmail.com>
2022-05-03 12:57:40 +0200

    fixes

--- React_Lucky_Frontend/src/shared/components/InfoAdiccional/InfoAdiccional.jsx
+++ React_Lucky_Frontend/src/shared/components/InfoAdiccional/InfoAdiccional.jsx
@@ -12,11 +12,7 @@ const InfoAdiccional = ({ mascotaDetail }) => {
           <h3 className="c-infoAdiccional__box__text">Subir imágenes</h3>
         </div>
         <div>
-          <p className="c-infoAdiccional__box__littleText">
-            Necesitamos que nos subas algunas fotos de dónde va a vivir tu nueva
-            mascota para poder echarte una mano si necesitas algo más de
-            información
-          </p>
+          <p className="c-infoAdiccional__box__littleText">Necesitamos que nos subas algunas fotos de dónde va a vivir tu nueva mascota para poder echarte una mano si necesitas algo más de información</p>
         </div>
         <div className="c-infoAdiccional__box__imgBox">
           <img className="c-infoAdiccional__box__img" src={Galeria} alt="" />
@@ -25,44 +21,27 @@ const InfoAdiccional = ({ mascotaDetail }) => {
 
       <div className="c-infoAdiccional__box">
         <div>
-          <h3 className="c-infoAdiccional__box__text c-infoAdiccional__box__text--mod">
-            ¿Cómo quieres pagar las tasas?
-          </h3>
+          <h3 className="c-infoAdiccional__box__text c-infoAdiccional__box__text--mod">¿Cómo quieres pagar las tasas?</h3>
         </div>
         <div>
-          <p className="c-infoAdiccional__box__littleText">
-            Para pagar las tasas de adopción puedes elegir o pagarlo mediante la
-            app con un pago único o poniéndose en contacto con la protectora
-            para fraccionar el pago
-          </p>
+          <p className="c-infoAdiccional__box__littleText">Para pagar las tasas de adopción puedes elegir o pagarlo mediante la app con un pago único o poniéndose en contacto con la protectora para fraccionar el pago</p>
         </div>
         <div></div>
         <div className="c-profile-top__button c-infoAdiccional__box--mod">
           <div className="c-profile-top__button__left">
             <p className="c-profile-top__button__p">
-              {mascotaDetail.tasa} € Desglosar las tasas
+              {mascotaDetail.tasa} € <span className="c-infoAdiccional__box__span">Desglosar las tasas</span>
             </p>
           </div>
           <div className="c-profile-top__button__right">
-            <img
-              className="c-infoAdiccional__box__flecha"
-              src={FlechaDown}
-              alt="icon"
-            />
+            <img className="c-infoAdiccional__box__flecha" src={FlechaDown} alt="icon" />
           </div>
         </div>
         <div className="c-infoAdiccional__box__checkboxes">
           <div className="c-infoAdiccional__box__checkboxes__solo">
-            <input
-              className="c-infoAdiccional__box__checkboxes__solo__checkbox"
-              type="checkbox"
-            />
+            <input className="c-infoAdiccional__box__checkboxes__solo__checkbox" type="checkbox" />
             <p>Pagar a través de la aplicación</p>
-            <img
-              className="c-infoAdiccional__box__checkboxes__solo__img"
-              src={Info}
-              alt=""
-            />
+            <img className="c-infoAdiccional__box__checkboxes__solo__img" src={Info} alt="" />
           </div>
         </div>
       </div>
